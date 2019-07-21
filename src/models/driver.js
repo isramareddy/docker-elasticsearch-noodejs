b88e00a3158700e31cdf229e521cbab90a158fde
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-21 20:26:20 +0100

    added driver model.

---
+++ src/models/driver.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const mongoosastic = require('mongoosastic');
+
+const { Schema } = mongoose;
+
+
+const driverSchema = new Schema({
+    full_name: {
+        type: String,
+        default: '',
+        es_indexed:true
+    },
+    email: {
+        type: String,
+        default: ''
+    },
+    phone: {
+        type: String,
+        default: ''
+    }
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+driverSchema.plugin(mongoosastic, {
+    hosts: [
+        'localhost:9200',
+    ]
+});
+
+const Driver = mongoose.model('Driver', driverSchema);
+
+
+module.exports = Driver;
