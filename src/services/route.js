f7e1b76b30836c33f75aaa9e907874e83841c92c
mohamedbrahimi <mohamedaissabrahimi@gmail.com>
2019-07-30 19:55:12 +0100

    implemented new endpoint for es search with pagination.

--- src/services/route.js
+++ src/services/route.js
@@ -4,7 +4,11 @@ import {
          useHydrateEsSearch,
          useHydrateWithOptionsEsSearch,
          useHydrateWithOptionsWithESResults
-       } from '../controllers/es-search';
+       } from '../controllers/es/es-search';
+
+import {
+         usePagination
+       } from '../controllers/es/advanced-es-search';
 
 const router = Router();
 
@@ -20,6 +24,10 @@ router.route('/es/use_hydrate_options')
 
 router.route('/es/use_hydrate_with_es_results')
     .get(useHydrateWithOptionsWithESResults);
+
+router.route('/es/use_pagination')
+    .get(usePagination);
+
 
 
 export default router;
